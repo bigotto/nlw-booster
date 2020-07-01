2e7a59a4979c480d71fd9dd885a00e8d0cac2ae7
Bruno Bigotto <brunobigotto@hotmail.com>
2020-07-01 09:14:13 -0400

    Adicionando o banco, as rotas do backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
-const port = 3333;
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.get('/users', (req, res) => {
-    res.json([
-        "Bruno",
-        "Gustavo",
-        "Bigotto",
-    ]);
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-
-app.listen(port, () => {
-    console.log('Server running on port ' + port)
-})
+app.listen(3333, () => {
+    console.log('Server running on port ' + 3333)
+});
